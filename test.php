ef3b7bead46f1c794385ce0138bd510570b13066
SHUVO BISWAS <76837017+g0y3nd4@users.noreply.github.com>
2024-12-22 02:11:47 +0530

    Update and rename test.php to test.ns

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-ob_start();
-phpinfo();  // Capture the phpinfo() output
-$phpinfo = ob_get_contents();
-ob_end_clean();
-
-// Base64 encode the PHP info output
-echo base64_encode($phpinfo);
-?>
